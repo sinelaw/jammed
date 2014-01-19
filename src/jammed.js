67e4b986b7ed539364940dd25db74f6c323d6fe6
Noam Lewis <jones.noamle@gmail.com>
2014-01-19 00:22:44 -0500

    initial code commit

---
+++ src/jammed.js
@@ -0,0 +1,90 @@
+var jammed = (function () {
+    'use strict';
+
+    var intervalId;
+
+    function Vector(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+
+    function Car(size, position, velocity) {
+        this.size = size;
+        this.position = position;
+        this.velocity = velocity;
+    }
+
+    function createWorld(width, height) {
+        return {
+            width: width,
+            height: height,
+            cars: []
+        };
+    }
+
+    function getCanvas() {
+        return document.getElementById('canvas');
+    }
+
+    function getContext(canvas) {
+        return canvas.getContext('2d');
+    }
+
+    function drawWorld(world) {
+        var i;
+        var canvas = getCanvas();
+        var context = getContext(canvas);
+        var car;
+        for (i = 0; i < world.cars.length; i += 1) {
+            car = world.cars[i];
+            context.fillRect(car.position.x, car.position.y, car.size.x, car.size.y);
+
+        }
+    }
+
+    function addRandomCar(world) {
+        var maxVelSqrt = 10;
+        world.cars.push(new Car(new Vector(1, 2),
+            new Vector(Math.random() * world.width, Math.random() * world.height),
+            new Vector(Math.random() * maxVelSqrt, Math.random() * maxVelSqrt)));
+    }
+
+    function stop() {
+        if (intervalId) {
+            window.clearInterval(intervalId);
+        }
+    }
+
+    return {
+        start: function () {
+            var canvas = getCanvas();
+            var world = createWorld(canvas.width, canvas.height);
+            var i;
+
+            for (i = 0; i < 100; i += 1) {
+                addRandomCar(world);
+            }
+
+            intervalId = window.setInterval(function () {
+                drawWorld(world);
+            }, 100);
+        },
+
+        stop: stop,
+
+        clear: function () {
+            var canvas = getCanvas();
+            var context = getContext(canvas);
+            // Store the current transformation matrix
+            context.save();
+
+            // Use the identity matrix while clearing the canvas
+            context.setTransform(1, 0, 0, 1, 0, 0);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            // Restore the transform
+            context.restore();
+        }
+    };
+
+}());
